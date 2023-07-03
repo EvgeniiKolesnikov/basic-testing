38a81e0cafc6343a0c91c7d9d3c11fce30896b3d
Evgenii Kolesnikov <jekow@yandex.ru>
2023-07-03 18:38:33 +0300

    feat: simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,65 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 1, b: 1, action: Action.Add })).toBe(2);
+    expect(simpleCalculator({ a: 2, b: 2, action: Action.Add })).toBe(4);
+    expect(simpleCalculator({ a: 0, b: 4, action: Action.Add })).toBe(4);
+    expect(simpleCalculator({ a: 5, b: 0, action: Action.Add })).toBe(5);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 1, b: 1, action: Action.Subtract })).toBe(0);
+    expect(simpleCalculator({ a: 2, b: 2, action: Action.Subtract })).toBe(0);
+    expect(simpleCalculator({ a: 0, b: 4, action: Action.Subtract })).toBe(-4);
+    expect(simpleCalculator({ a: 5, b: 0, action: Action.Subtract })).toBe(5);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 1, b: 1, action: Action.Multiply })).toBe(1);
+    expect(simpleCalculator({ a: 2, b: 2, action: Action.Multiply })).toBe(4);
+    expect(simpleCalculator({ a: 0, b: 4, action: Action.Multiply })).toBe(0);
+    expect(simpleCalculator({ a: 5, b: 0, action: Action.Multiply })).toBe(0);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 1, b: 1, action: Action.Divide })).toBe(1);
+    expect(simpleCalculator({ a: 2, b: 2, action: Action.Divide })).toBe(1);
+    expect(simpleCalculator({ a: 0, b: 4, action: Action.Divide })).toBe(0);
+    expect(simpleCalculator({ a: 5, b: 0, action: Action.Divide })).toBe(
+      Infinity,
+    );
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 1, b: 1, action: Action.Exponentiate })).toBe(
+      1,
+    );
+    expect(simpleCalculator({ a: 2, b: 2, action: Action.Exponentiate })).toBe(
+      4,
+    );
+    expect(simpleCalculator({ a: 0, b: 4, action: Action.Exponentiate })).toBe(
+      0,
+    );
+    expect(simpleCalculator({ a: 5, b: 0, action: Action.Exponentiate })).toBe(
+      1,
+    );
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const unknownAction: unknown = 'unknownAction';
+    expect(simpleCalculator({ a: 1, b: 1, action: unknownAction })).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const unknownA: unknown = 'unknownA';
+    const unknownB: unknown = 'unknownB';
+    expect(simpleCalculator({ a: unknownA, b: 1, action: Action.Add })).toBe(
+      null,
+    );
+    expect(simpleCalculator({ a: 2, b: unknownB, action: Action.Divide })).toBe(
+      null,
+    );
   });
 });
